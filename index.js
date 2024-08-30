52a5ac29bce061fd9c1751ffe28a07aa9d9fb8be
krishnapriya U.A <krishnapriyaua@gmail.com>
2024-08-30 12:23:45 +0530

    port changed

--- index.js
+++ index.js
@@ -108,7 +108,7 @@ app.use((req, res, next) => {
     next();
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
